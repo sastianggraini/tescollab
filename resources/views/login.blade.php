517b8a9745af1da53c3bdc0b50d670d0512e371d
Raynaldi984 <dhaniraynaldi984@gmail.com>
2024-03-06 15:30:45 +0700

    update login blade php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Login Page</title>
 </head>
 <body>
+  <div class="container">
+    <form id="loginForm">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
 
-    <h2>Login Form</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-
-        <input type="submit" value="Login">
+      <button type="button" onclick="validateLogin()">Login</button>
     </form>
+  </div>
 
+  <script src="script.js"></script>
 </body>
 </html>
